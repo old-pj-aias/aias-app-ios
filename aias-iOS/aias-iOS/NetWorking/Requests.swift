f0f471f6aba3cd8ffb12c546aae750479d5a06fd
shunta nakajima <nakg_asimo@icloud.com>
2020-09-01 16:31:31 +0900

    add signature Manager

--- aias-iOS/aias-iOS/NetWorking/Requests.swift
+++ aias-iOS/aias-iOS/NetWorking/Requests.swift
@@ -11,16 +11,18 @@ import RxAlamofire
 import Alamofire
 
 class AiasRequest {
-    private let baseAPIURLString = "localhost:8080"
+    static let shared = AiasRequest()
     
-    func request(body: String,path:AiasRequestPath,disposeBag: DisposeBag) -> Observable<String>{
+    private let baseAPIURLString = "http://localhost:8080/"
+    
+    func request(body: String,path:AiasRequestPath) -> Observable<String>{
         let url = baseAPIURLString + path.rawValue
         var request = URLRequest(url: URL(string: url)!)
         request.httpBody = body.data(using: .utf8)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         return RxAlamofire.requestData(request as URLRequestConvertible).map{ (response, data) -> String in
-            return String(data: data, encoding: .utf8)!
+            String(data: data, encoding: .utf8)!
         }
     }
     
