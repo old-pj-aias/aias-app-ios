7e7dfe2d20fffb6e504fc1187bd183bcef112ee8
shunta nakajima <nakg_asimo@icloud.com>
2020-09-05 02:18:09 +0900

    change judge name

--- aias-iOS/aias-iOS/Utility/Judgekeys.swift
+++ aias-iOS/aias-iOS/Utility/Judgekeys.swift
@@ -21,7 +21,7 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
 -----END PUBLIC KEY-----
 """
     let keys = [
-        EJkey(name: "1", key:
+        EJkey(name: "akakou", key:
             """
 -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
@@ -34,8 +34,21 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
 -----END PUBLIC KEY-----
 """
         ),
-        EJkey(name: "2", key:
-    """
+        EJkey(name: "shunta", key:
+            """
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
+Zac8NWskP59EAVFlO218xIXOV0FfphPB/tnbQh7GDXddo7XVEptHdHXyJlXXLihb
+9vXbUZF2NDFLOhgDv7pa72VNLbw+jKR/FlsDtwv/bv7ZDqq+n79uavuJ8giX3qCf
++mtBmro7hG5AVve3JImhvA0FvTKJ0xCYUYw02st08He5RwFAXQK8G2cwahp+5ECH
+MDdfFUaoxMfRN/+Hl9iqiJovKUJQ3545N2fDYdd0eqSlqL1N5xJxYX1GDMtGZgME
+hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
+/wIDAQAB
+-----END PUBLIC KEY-----
+"""
+        ),
+        EJkey(name: "makino", key:
+            """
 -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
 Zac8NWskP59EAVFlO218xIXOV0FfphPB/tnbQh7GDXddo7XVEptHdHXyJlXXLihb
